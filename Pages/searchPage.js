2083bd2089162457a5ed54aa959ecbc514d91eb2
igniteram <ram.pasala7@gmail.com>
2016-06-18 15:04:13 +0530

    changing browser to firefox

--- Pages/searchPage.js
+++ Pages/searchPage.js
@@ -9,7 +9,7 @@ var googleSearch = function () {
     };
     this.searchButton = $("button[name='btnG']");
     this.clearText = function () {
-        $("input[name='q1']").clear();
+        $("input[name='q']").clear();
     };
 };
 module.exports = googleSearch;
